ee93bf0112e06bafa937b45c79e402a8a2af330f
arslanbekova <lyuda.arslanbekova@gmail.com>
2020-07-18 00:28:13 +0300

    stylelint fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 (function () {
-window.util.setOrRemoveAttribute(window.announcementFormFields, 'disabled', true);
-window.util.setOrRemoveAttribute(window.filterFormFields, 'disabled', true);
+  window.util.setOrRemoveAttribute(window.announcementFormFields, 'disabled', true);
+  window.util.setOrRemoveAttribute(window.filterFormFields, 'disabled', true);
 
-window.util.addEventListenerOnce(window.mapPinMain, 'mousedown', window.activatePage);
-window.util.addEventListenerOnce(window.mapPinMain, 'keydown', window.activatePage);
+  window.util.addEventListenerOnce(window.mapPinMain, 'mousedown', window.activatePage);
+  window.util.addEventListenerOnce(window.mapPinMain, 'keydown', window.activatePage);
 
-window.similarListPin.addEventListener('click', window.renderAnnouncementPopup);
-window.similarListPin.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, window.renderAnnouncementPopup);
-});
+  window.similarListPin.addEventListener('click', window.renderAnnouncementPopup);
+  window.similarListPin.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, window.renderAnnouncementPopup);
+  });
 })();
